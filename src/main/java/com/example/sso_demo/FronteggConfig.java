2c6496211d565d1b3c89a465dc6684cbee604a2e
Eduardo Murillo <eduardomurillo@Eduardos-MacBook-Pro.local>
2024-12-31 14:51:36 -0600

    Initial commit

---
+++ src/main/java/com/example/sso_demo/FronteggConfig.java
@@ -0,0 +1,9 @@
+package com.example.sso_demo;
+
+import com.frontegg.sdk.api.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class FronteggConfig {
+
+}
---
+++ src/test/java/com/example/sso_demo/SsoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sso_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
